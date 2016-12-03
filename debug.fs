3e901c8ff3e0be66afafd1a48e9d902272f93407
Phil Krylov <phil.krylov@gmail.com>
2016-12-03 21:30:51 +0300

    Added stack depth checking words \stack-mark and \stack-check

--- debug.fs
+++ debug.fs
@@ -8,6 +8,16 @@ VARIABLE n-forms
 2VARIABLE timer
 
 0 VALUE debug-mode?
+CREATE depth-stack 256 CELLS ALLOT
+0 VALUE depth-stack-depth
+: depth-stack-push  ( -- )
+  DEPTH depth-stack depth-stack-depth CELLS + !
+  depth-stack-depth 1+ TO depth-stack-depth ;
+: depth-stack-check ( -- )
+  depth-stack-depth 1- TO depth-stack-depth
+  DEPTH depth-stack depth-stack-depth CELLS + @ <> IF
+    ABORT" UNEXPECTED STACK DEPTH CHANGE!"
+  THEN ;
 
 : debug-init
   utime timer 2! ;
@@ -29,3 +39,10 @@ DEFER debug-bye
 : .as  ( -- )
   \." AS:" slot-stack DUP @ . CELL+ BEGIN DUP @ DUP . WHILE CELL+ REPEAT DROP CR
   ;
+
+: \stack-mark  ( -- )
+  debug-mode? IF POSTPONE depth-stack-push THEN ; immediate
+
+: \stack-check  ( -- )
+  debug-mode? IF POSTPONE depth-stack-check THEN ; immediate
+
