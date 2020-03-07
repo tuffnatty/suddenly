1c044176e8ee6977cbff80133aba7195ae69c2c9
Phil Krylov <phil.krylov@gmail.com>
2020-03-07 20:43:35 +0300

    debugging: better backtrace on stack check, add timers

--- debugging.fs
+++ debugging.fs
@@ -21,12 +21,14 @@ CREATE depth-stack depth-stack-limit CELLS ALLOT
   depth-stack-depth depth-stack-limit = ABORT" DEPTH STACK OVERFLOW!"
   DEPTH depth-stack depth-stack-depth CELLS + !
   depth-stack-depth 1+ TO depth-stack-depth ;
-: depth-stack-check ( -- )
+: depth-stack-check ( -- f )
   depth-stack-depth 1- TO depth-stack-depth
+  depth-stack-depth 0< ABORT" DEPTH STACK OVERFLOW!"
+  \ depth-stack-depth 0 DO BL EMIT LOOP ." \stack-check" CR
   DEPTH depth-stack@ <> IF
-    ." UNEXPECTED STACK DEPTH CHANGE! (" DEPTH . ." instead of " depth-stack@ . ." )" CR
-    -1 THROW
-  THEN ;
+    ." UNEXPECTED STACK DEPTH CHANGE! (" DEPTH . ." instead of " depth-stack@ . ." )"
+    TRUE
+  ELSE FALSE THEN ;
 
 : debug-init
   utime timer 2! ;
@@ -45,7 +47,9 @@ DEFER debug-bye
   THEN ; IMMEDIATE
 
 : \.s
-  debug-mode? IF  [CHAR] < ]]L EMIT DEPTH . [[ [CHAR] > ]]L EMIT DUP . [[ $2026 ]]L XEMIT CR [[ THEN ; IMMEDIATE
+  debug-mode? IF
+    [CHAR] < ]]L EMIT  DEPTH .  [[ [CHAR] > ]]L EMIT  DUP .  [[ $2026 ]]L XEMIT  CR [[
+  THEN ; IMMEDIATE
 
 : \\." debug-mode? 1 > IF POSTPONE ." ELSE POSTPONE \ THEN ; IMMEDIATE
 : .as  ( -- )
@@ -53,10 +57,21 @@ DEFER debug-bye
   ;
 
 : \stack-mark  ( -- )
-  debug-mode? IF POSTPONE depth-stack-push THEN ; immediate
+  debug-mode? IF  ]] depth-stack-push                [[  THEN ; IMMEDIATE
 
 : \stack-check  ( -- )
-  debug-mode? IF POSTPONE depth-stack-check THEN ; immediate
+  debug-mode? IF  ]] depth-stack-check IF WTF?? THEN [[  THEN ; IMMEDIATE
+
+variable last-timer
+CREATE timer-stack 256 CELLS ALLOT
+variable timer-stack-depth
+: timer: Create $0. , , here timer-list !@ ,
+  DOES> profile(
+    timer-stack-depth @ ?DUP-IF 1- CELLS timer-stack + @  +t THEN
+    timer-stack timer-stack-depth @ CELLS + !
+    1 timer-stack-depth +!
+  )else( drop ) ;
+: +record ( -- ) profile( -1 timer-stack-depth +!  timer-stack timer-stack-depth @ CELLS + @  +t ) ;
 
 [undefined] SEE-THREADED [IF]
 [undefined] see-voc [if]
@@ -66,4 +81,4 @@ ALSO see-voc
 : see-threaded SEE-THREADED ;
 PREVIOUS
 [then]
-[THEN]
+
