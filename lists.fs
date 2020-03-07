a305438cea908ab77574a7fb4416292e8e16be00
Phil Krylov <phil.krylov@gmail.com>
2020-03-07 20:30:48 +0300

    Add list-any? strlist-maxlen strlist-prefix-in?

--- lists.fs
+++ lists.fs
@@ -14,6 +14,11 @@ END-STRUCT list%
     R@ SWAP >R R@ SWAP EXECUTE R>
   list-next @ REPEAT DROP RDROP ;
 
+: list-any?  ( ... list xt -- ... )
+  >R BEGIN DUP WHILE
+    R@ SWAP >R R@ SWAP EXECUTE ?DUP-IF 2RDROP EXIT THEN R>
+  list-next @ REPEAT DROP RDROP FALSE ;
+
 
 list%
   CELL%      FIELD ptrlist-ptr
@@ -59,22 +64,39 @@ END-STRUCT strlist%
   strlist% %ALLOC strlist-prepend-common
   ;
 
+: strlist-maxlen  ( list -- addr ) 63 CELLS ]]L + [[ ; IMMEDIATE
+
 : strlist-parse-alloc  { list addr u -- list' }
+  0 { maxlen }
   BEGIN u DUP 0> WHILE  ( u )
     addr SWAP BEGIN OVER C@ BL <> WHILE
       1 /STRING
     REPEAT  ( addr' u' )
     u OVER - list SWAP addr SWAP strlist-prepend-alloc TO list
+    list strlist-str C@  DUP maxlen > IF  list strlist-maxlen !  ELSE DROP THEN
     1 /STRING
     TO u  TO addr
   REPEAT
   list ;
 
 : strlist-in?  ( addr u list -- f )
-  BEGIN DUP WHILE  { list' }                          ( addr u )
-    list' strlist-get  2OVER STR= IF  2DROP TRUE EXIT  THEN
-    list' list-next @                           ( addr u list' )
-  REPEAT DROP 2DROP FALSE ;
+  2DUP strlist-maxlen @ <= IF
+    BEGIN DUP WHILE  { list' }                          ( addr u )
+      list' strlist-get  2OVER STR= IF  2DROP TRUE EXIT  THEN
+      list' list-next @                           ( addr u list' )
+    REPEAT
+  THEN
+  DROP 2DROP FALSE ;
+
+: strlist-prefix-in?  ( addr u list -- f )
+  2DUP strlist-maxlen @ >= IF
+    NIP DUP strlist-maxlen @ SWAP  ( addr u' list )
+    BEGIN DUP WHILE  { list' }                          ( addr u )
+      list' strlist-get  2OVER STR= IF  2DROP TRUE EXIT  THEN
+      list' list-next @                           ( addr u list' )
+    REPEAT
+  THEN
+  DROP 2DROP FALSE ;
 
 : strlists-intersect?  ( list1 list2 -- f )
   { list2 }                       ( list1 )
