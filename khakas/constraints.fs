tuffnatty/suddenly

commit ccd9cc7c45f2fb9542cd6082017a0d87269835bb

(#213) Added VGV+Voice fallout constraint

khakas/constraints.fs
@@ -678,6 +678,18 @@ require khakas/slotnames.fs
   first-affix t~/ {short-vowel} NOT
   ;
 
+\ 3.1. ...
+\ С залоговыми показателями невозможно стяжение от основ вида
+\ VC, ср. оол ‘мальчик’, но не #оғ- /оң- + (Ы)л.
+\ С основами CVC стяжение обязательно: чуунарға ‘мыться’ < чуғ
+\ ‘мыть’. 
+: constraint-Voice-VГV-fallout  ( -- f )
+  stem-polysyllabic? ||
+  guessed-stem first-sound vowel?
+  first-form-flag untransformed-fallout-VГV untransformed-fallout-VңV OR AND
+  logical-xor
+  ;
+
 \ Запрещенные контексты для выпадения конечного к, х
 : constraint-V[кх]V-fallout  ( -- f )
   first-form-flag untransformed-fallout-V[кх]V AND NOT  ||

khakas/gentest.fs
@@ -100,6 +100,8 @@ T{ S" ойла+а+м"			S" ойлаам"		parse-test -> FALSE }T
 T{ S" ойна+а+быс"		S" ойнаабыс"		parse-test -> FALSE }T
 T{ S" олар+дың"			S" олардың"		parse-test -> TRUE }T
 T{ S" ол+ох"			S" олох"		parse-test -> TRUE }T
+T{ S" оғ+ыл"			S" оол"			parse-test -> FALSE }T  \ #213
+T{ S" оң+ыл"			S" оол"			parse-test -> FALSE }T  \ #213
 T{ S" оолғы+м+ай"		S" оолғымай"		parse-test -> TRUE }T
 T{ S" орын+ы"			S" орны"		parse-test -> TRUE }T
 T{ S" орын+ы+лар+ы"		S" орнылары"		parse-test -> TRUE }T
@@ -359,6 +361,8 @@ T{ S" чӧлег+ім"			S" чӧлеем"		parse-test -> TRUE }T
 T{ S" чӧп+тее+біс"		S" чӧптеебіс"		parse-test -> FALSE }T  \ #197
 T{ S" чӧр+беен+іс+тір"		S" чӧрбееністір"	parse-test -> TRUE }T
 T{ S" чӧр+ӌең+і"		S" чӧрӌее"		parse-test -> TRUE }T
+T{ S" чуғ+ын+ар+ға"		S" чуунарға"		parse-test -> TRUE }T  \ #213
+T{ S" чуғ+ын+ар+ға"		S" чуғынарға"		parse-test -> FALSE }T  \ #213
 T{ S" чурта+п+ох+ча+быс"	S" чуртабохчабыс"	parse-test DROP expected-found -> 1 }T  \ #111
 T{ S" чурта+п+чадыр"		S" чуртапчадыр"		parse-test -> TRUE }T
 T{ S" чӱг+і"			S" чӱгі"		parse-test -> TRUE }T

khakas/paradigms.fs
@@ -47,12 +47,14 @@ VARIABLE slot-stack-here  slot-stack slot-stack-here !
   form" -novoice "
 
   <this> slot-full!
-  form" Pass (Ы)л"
-  form" Refl (Ы)н"
-  form" Rec (Ы)с"
-  form" Caus ТЫр"
-  filters( constraint-36 )
-    form" Caus т"
+  filters( constraint-Voice-VГV-fallout )
+    form" Pass (Ы)л"
+    form" Refl (Ы)н"
+    form" Rec (Ы)с"
+    form" Caus ТЫр"
+    filters( constraint-36 )
+      form" Caus т"
+    filters-end
   filters-end
   ; slot-add
 

khakas/transforms.fs
@@ -566,12 +566,14 @@ end-public-class Untransformer
       \ многосложные: теен ‘сказал’ < тi ‘сказать’ + ған Past,
       \ чеелек ‘еще не поел’ < чi ‘есть’ + гелек Cunc.
       affix t~/ г|ғ IF
+        untransformed-fallout-VГV TO flags
         "і" C   V1 unfallout-add-vcv
       THEN
 
       \ В прочих случаях на месте стяжения образуется длинная
       \ гласная, идентичная корневой.
       vowels sound-each-str { D: V2 }
+        untransformed-fallout-VГV TO flags
         V1  C   V2 unfallout-add-vcv
         untransformed-fallout-VңV TO flags
         V1  "ң" V2 unfallout-add-vcv

util.fs
@@ -197,6 +197,9 @@ DEFER xemit
 : &&  ( f R: x r -- false R: x PC: r | R: x r )  \ AND with boolean shortcircuiting
   ]] ?DUP-0=-IF FALSE EXIT ELSE DROP THEN [[ ; IMMEDIATE COMPILE-ONLY
 
+: logical-xor  ( f f -- f )
+  0<> SWAP 0<> XOR ;
+
 : array-reverse  ( arr len -- )
   1- CELLS OVER + BEGIN 2DUP < WHILE  ( a1 a2 )
     2DUP 2DUP @ SWAP @ ROT ! SWAP !

commit 46fa7376d5923e1f785e9ae37fca641523f12595

(#231) Fixed Voices broken with ccd9cc7

khakas/constraints.fs
@@ -716,6 +716,7 @@ require khakas/slotnames.fs
 \ С основами CVC стяжение обязательно: чуунарға ‘мыться’ < чуғ
 \ ‘мыть’. 
 : constraint-Voice-VГV-fallout  ( -- f )
+  stem-last-sound gh-g-ng? NOT ||
   stem-polysyllabic? ||
   guessed-stem first-sound vowel?
   first-form-flag untransformed-fallout-VГV untransformed-fallout-VңV OR AND

khakas/gentest.fs
@@ -81,6 +81,8 @@ T{ S" кӧзӧ+ге"			S" кӧзӧге"		parse-test -> TRUE }T
 T{ S" кӧй+0︀+чары"		S" кӧйчары"		parse-test -> TRUE }T  \ #181
 T{ S" кӧр+бе+ген+дее+бін"	S" кӧрбеендеебін"	parse-test -> TRUE }T
 T{ S" кӧр+еліс"			S" кӧреліс"		parse-test -> TRUE }T
+T{ S" кӧр+іл"			S" кӧріл"		parse-test -> TRUE }T  \ #231
+T{ S" кӧр+іс+кен"		S" кӧріскен"		parse-test -> TRUE }T  \ #231
 T{ S" курс+ы"			S" курзы"		parse-test -> TRUE }T
 T{ S" курс+ы"			S" курсы"		parse-test -> TRUE }T
 T{ S" кӱн+і+нге"		S" кӱнінге"		parse-test -> TRUE }T
@@ -206,6 +208,7 @@ T{ S" сағыссырас+та+зар"	S" сағыссырастазар"	parse
 T{ S" сап+ыбыс"			S" саабыс"		parse-test -> TRUE }T
 T{ S" сап+ыбыс+хан"		S" саабысхан"		parse-test -> TRUE }T
 T{ S" сағ+ып"			S" саап"		parse-test -> TRUE }T
+T{ S" сал+ын+ған"		S" салынған"		parse-test -> TRUE }T  \ #231
 T{ S" салаа+ға"			S" салааға"		parse-test -> TRUE }T
 T{ S" самолёт+ы"		S" самолёды"		parse-test -> TRUE }T
 T{ S" сана"			S" сана"		parse-test -> TRUE }T
@@ -297,6 +300,7 @@ T{ S" турлах+а"			S" турлаа"		parse-test -> FALSE }T  \ #213
 T{ S" туза+лығ+лар"		S" тузалығлар"		parse-test DROP expected-found -> 2 }T
 T{ S" тус+ы"			S" тузы"		parse-test -> TRUE }T
 T{ S" тут+ар+ға"		S" тударға"		parse-test -> TRUE }T
+T{ S" тӱс+іс"			S" тӱзіс"		parse-test -> TRUE }T  \ #231
 T{ S" тыы+п+ла+ыс+хан"		S" тыыплаысхан"		parse-test -> TRUE }T
 T{ S" ӱг+ер"			S" ӱӱр"			parse-test -> TRUE }T
 T{ S" ӱг+іп"			S" ӱӱп"			parse-test -> TRUE }T
@@ -357,6 +361,7 @@ T{ S" часка+лығ+ға"		S" часкалыға"		parse-test -> TRUE }T
 T{ S" чі+еді+ңер"		S" чидер"		parse-test -> FALSE }T  \ #212
 T{ S" чі+еді+п"			S" чидіп"		parse-test -> FALSE }T  \ #209
 T{ S" чит+іп"			S" чидіп"		parse-test -> TRUE }T  \ #209
+T{ S" чіт+іс"			S" чідіс"		parse-test -> TRUE }T  \ #231
 T{ S" чі+гелек"			S" чеелек"		parse-test -> TRUE }T
 T{ S" чилін+і"			S" чилні"		parse-test -> TRUE }T
 T{ S" чі+е+м+дір"		S" чимдір"		parse-test -> TRUE }T